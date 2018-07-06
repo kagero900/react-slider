6e6fa66c77b3e2b6bbf2b0bf10fdb842fd6850e4
Kagero <lj.chistova@yandex.ru>
2018-07-06 08:42:13 +0300

    environment setting

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import Grid from 'react-bootstrap/lib/Grid';
+import Row from 'react-bootstrap/lib/Row';
+import Col from 'react-bootstrap/lib/Col';
+import styled from 'styled-components';
+
+class App extends Component {
+  render() {
+    return (
+            <div className = "wrapper">       
+              <SayFullName name="Kagero" surname="Orgrimmarskaya" link="https://vk.com/kagero900" />
+              <SayFullName name="Asgerd" surname="dinAlt" link="https://facebook.com" />
+              <SayFullName name="Thrall" surname="Durotan" link="https://blizzard.com" />
+            </div>
+    );
+  }
+}
+
+function SayFullName(props) {
+  return (
+      <div>
+      <h1>Мое имя {props.name}, фамилия - {props.surname}</h1>
+      <a href={props.link}> Ссылка на мой профиль </a>
+      </div>
+    )
+}
+
+export default App;
